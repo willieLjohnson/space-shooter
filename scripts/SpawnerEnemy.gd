0911a567c51d5322a1938af0f6152e92097ae076
willieLjohnson <liwa.johnson@gmail.com>
2020-05-28 13:49:13 -0400

    Create randomized enemy spanwer script

---
+++ scripts/SpawnerEnemy.gd
@@ -0,0 +1,17 @@
+extends Node
+
+const ENEMY_SIZE = 16
+
+const enemy_kamikaze_scene = preload("res://scenes/EnemyKamikaze.tscn")
+
+func _ready() -> void:
+	spawn()
+	
+func spawn() -> void:
+	randomize()
+	var enemy = enemy_kamikaze_scene.instance()
+	var position = Vector2()
+	position.x = rand_range(0 + ENEMY_SIZE, get_viewport().get_visible_rect().size.x - ENEMY_SIZE)
+	position.y = 0 - ENEMY_SIZE
+	enemy.set_position(position)
+	get_node("Container").add_child(enemy)
