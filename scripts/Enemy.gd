28857218c2c0dca2163d8e1b19ae2b4e82b8bd3c
willieLjohnson <liwa.johnson@gmail.com>
2020-05-29 10:44:33 -0400

    Create enemy clever script

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-const HEIGHT = 16
+const SIZE = 16
 
 export var velocity = Vector2()
 
@@ -11,5 +11,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	translate(velocity * delta)
 	
-	if get_position().y - HEIGHT >= get_viewport_rect().size.y:
+	if get_position().y - SIZE >= get_viewport_rect().size.y:
 		queue_free()
---
+++ scripts/EnemyClever.gd
@@ -0,0 +1,22 @@
+extends "res://scripts/Enemy.gd"
+
+const laser_scene = preload("res://scenes/LaserEnemy.tscn")
+
+func _ready() -> void:
+	velocity.x = Utils.choose([velocity.x, -velocity.x])
+	
+	yield(Utils.create_timer(1), "timeout")
+	shoot()
+
+func _process(delta) -> void:
+	if get_position().x <= 0 + SIZE:
+		velocity.x = abs(velocity.x)
+	if get_position().x >= Utils.view_size.x - SIZE:
+		velocity.x = -abs(velocity.x)
+
+func shoot():
+	while true:
+		var laser = laser_scene.instance()
+		laser.set_position($Cannon.get_global_position)
+		Utils.main_node.add_child(laser)
+		yield(Utils.create_timer(1.5), "timeout")
