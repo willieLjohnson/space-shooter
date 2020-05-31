8f8fd8a9f23bdc5717e004bd85ec3af8e8ac3351
willieLjohnson <liwa.johnson@gmail.com>
2020-05-31 09:54:54 -0400

    Create double shooting powerup

---
+++ scripts/PowerupLaser.gd
@@ -0,0 +1,10 @@
+extends "res://scripts/Powerup.gd"
+
+
+func _ready() -> void:
+	connect("area_entered", self, "_on_area_entered")
+
+func _on_area_entered(other):
+	if other.is_in_group("Ship"):
+		other.is_double_shooting = true
+		queue_free()
